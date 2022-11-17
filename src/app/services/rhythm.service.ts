dfcd2b9e56c92a36bf7c410030a870d1f84c72dd
Lucero Raul <luceroraul12@gmail.com>
2022-11-17 19:24:17 -0300

    add method for generate list of rhythm

--- src/app/services/rhythm.service.ts
+++ src/app/services/rhythm.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Rhythm } from '../interfaces/phrase-response.interface';
+import { KindOfAccent, Rhythm } from '../interfaces/phrase-response.interface';
 import { PhraseService } from './phrase.service';
 
 @Injectable({
@@ -12,11 +12,41 @@ export class RhythmService {
   ) { }
 
   readPhrase(rhythms: Rhythm[]){
+    this.generateListOfRhythms(rhythms);
+
     rhythms.forEach((r,index) => {
         
         setTimeout(()=>{
           this.phraseService.communicator.next(r.wordIndexInPhrase);
         }, index * 1000);
     })
+  }
+
+  generateListOfRhythms(rhythms: Rhythm[]): Array<Rhythm[]>{
+    let highIndexs: number[] = [];
+    let result: Array<Rhythm[]> = [];
+
+    rhythms.forEach((p, index) => {
+      if (p.kindOfAccent == KindOfAccent.HIGH){
+        highIndexs.push(index);
+      }
+    });
+
+    console.log('index of High accents',highIndexs);
+    let lastIndex = 0;
+    highIndexs.forEach(
+      i => {
+        result.push(
+          rhythms.slice(lastIndex, i)
+        );
+        lastIndex = i;
+      }
+    );
+
+    console.log('result :', result);
+    
+
+
+    return [];
   }
 }
