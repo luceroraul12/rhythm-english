010052fc862e69137950fa2c92a16778bc6fa2e7
Lucero Raul <luceroraul12@gmail.com>
2022-11-17 17:35:44 -0300

    add example of backend response

--- src/app/services/phrase.service.ts
+++ src/app/services/phrase.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
+import { KindOfAccent, PhraseResponse, Rhythm } from '../interfaces/phrase-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhraseService {
-  phrase: string = "I like to play the piano all days in my home";
+  rhythmResponse: PhraseResponse = {
+    phrase: 'I like to play the piano all days in my home', // 11 words
+    rhythms: [
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 0},
+      {kindOfAccent: KindOfAccent.HIGH, wordIndexInPhrase: 1},
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 2},
+      {kindOfAccent: KindOfAccent.HIGH, wordIndexInPhrase: 3},
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 4},
+      {kindOfAccent: KindOfAccent.HIGH, wordIndexInPhrase: 5},
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 6},
+      {kindOfAccent: KindOfAccent.HIGH, wordIndexInPhrase: 7},
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 8},
+      {kindOfAccent: KindOfAccent.LOW, wordIndexInPhrase: 9},
+      {kindOfAccent: KindOfAccent.HIGH, wordIndexInPhrase: 10},
+    ]
+  }
 
   constructor() { }
 }
